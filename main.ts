c725a100ef83740cd1d6cafecda426a21c375820
DziveQ <56726128+DziveQ@users.noreply.github.com>
2020-12-08 13:47:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-let motor = 0;
-
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         if (motor == 1) {
-            bitbot.go(BBDirection.Forward, 100);
+            bitbot.go(BBDirection.Forward, 100)
         }
     } else if (receivedNumber == 0) {
         if (motor == 1) {
             bitbot.stop(BBStopMode.Brake)
         }
     } else if (receivedNumber == 2) {
-    	if (motor == 0) {
-            motor = 1;
-            
+        if (motor == 0) {
+            motor = 1
+            music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
         } else if (motor == 1) {
-            motor = 0;
+            motor = 0
+            music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
         }
     } else if (receivedNumber == 11) {
         bitbot.setLedColor(0xffffff)
+    } else if (receivedNumber == 12) {
+        bitbot.ledRainbow()
+    } else if (receivedNumber == 0) {
+        bitbot.ledClear()
     }
 })
+let motor = 0
 radio.setGroup(5)
