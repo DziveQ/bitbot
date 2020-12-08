cbcb3de8b4ced060a619091ae673aa012e8d3f06
DziveQ <56726128+DziveQ@users.noreply.github.com>
2020-12-08 13:42:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
+let motor = 0;
+
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        bitbot.go(BBDirection.Forward, 40)
+        if (motor == 1) {
+            bitbot.go(BBDirection.Forward, 100);
+        }
     } else if (receivedNumber == 0) {
-        bitbot.stop(BBStopMode.Coast)
+        if (motor == 1) {
+            bitbot.stop(BBStopMode.Brake)
+        }
+    } else if (receivedNumber == 2) {
+    	if (motor == 0) {
+            motor = 1;
+            
+        } else if (motor == 1) {
+            motor = 0;
+        }
+    } else if (receivedNumber == 11) {
+        bitbot.setLedColor(0xffffff)
     }
 })
 radio.setGroup(5)
